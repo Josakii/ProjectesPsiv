855a8ce7072e90ed2bc07e0dff9d852f68fae5a4
Josep Maria Torres Ruiz <160509345+Josakii@users.noreply.github.com>
2024-09-25 22:51:44 +0200

    Update Lectura_De_Matricula.py

--- codis/Lectura_De_Matricula.py
+++ codis/Lectura_De_Matricula.py
@@ -8,15 +8,15 @@ sys.path.append(yolo_repo_path)
 import torch
 from models.common import DetectMultiBackend
 
-# Asegúrate de usar la ruta como cadena para compatibilidad con Windows
+
 pt_model_path = 'C:/Users/Josep/Desktop/Matriculas Detection/weights/best.pt'
 
 # Cargar el modelo YOLOv5
 model = DetectMultiBackend(pt_model_path)  # Pasar la ruta directamente como cadena
-model.eval()  # Poner el modelo en modo evaluación
+model.eval()  
 print("Modelo YOLOv5 cargado:", model)
 
-# Preprocesamiento de imágenes
+
 from PIL import Image
 import numpy as np
 import torchvision.transforms as transforms
@@ -25,18 +25,18 @@ preprocess = transforms.Compose([
     transforms.ToTensor(),
 ])
 
-# Función para hacer predicciones con YOLOv5
+
 def predict_image_yolo(image_path):
     image = Image.open(image_path)
     input_tensor = preprocess(image).unsqueeze(0)  # Añadir la dimensión de batch
 
-    # Realizar la predicción
+
     with torch.no_grad():
         outputs = model(input_tensor)
 
     return outputs
 
-# Ejemplo de uso
+
 image_path = 'C:/Users/Josep/Desktop/Matriculas Detection/images/train/sample.jpg'
 outputs = predict_image_yolo(image_path)
 print("Predicciones:", outputs)
